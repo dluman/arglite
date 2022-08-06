ed5c856a555c60ae768b691b3c4a6ef4616824a8
dluman <dluman@allegheny.edu>
2022-08-05 21:46:01 -0400

    Refactoring the long description in the holy format

--- setup.py
+++ setup.py
@@ -8,22 +8,25 @@ setuptools.setup(
     include_package_data=True,
     description="A lightweight, dynamic arg parser for fun, but not profit.",
     long_description="""
-# arglite
+=======
+arglite
+=======
 
 A lightweight, dynamic argument parsing library for Python programs with klugy support for typing variables.
 
 I made this for a teaching machine project I'm working on (I needed a custom argument parser for _reasons_),
 and I'm always too impatient to use `argparse`.
 
-## Installation
+Installation
+============
 
 Find this tool on `PyPI`: `pip install arglite`
 
-## Usage
+Usage
+=====
 
 Check this out:
 
-```python
 import arglite
 
 def main():
@@ -35,15 +38,14 @@ def main():
 
 if __name__ == "__main__":
   main()
-```
 
-Run using `python main.py -a Yo --b that is -c`.
+Run using: python main.py -a Yo --b that is -c.
 
-## Notes
-
-* No automatic `-h` flag yet; soon?
-* Flags with no value are automatically converted to `True` boolean
-* The module uses `ast.literal_eval`, so `"{'a':'b'}"` will convert to a `dict` (all quotes required)    
+Notes
+=====
+No automatic `-h` flag yet; soon?
+Flags with no value are automatically converted to `True` boolean
+The module uses `ast.literal_eval`, so `"{'a':'b'}"` will convert to a `dict` (all quotes required)    
     """,
     long_description_content_type="text/x-rst",
     install_requires=[
