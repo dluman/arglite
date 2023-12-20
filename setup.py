1ffe4c08c004e7978f7b6a6ae82bc6d8f6307f92
dluman <dluman@allegheny.edu>
2023-12-20 16:58:52 -0500

    Updating version number for pre-PyPI shipment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="arglite",
-    version="0.6.0",
+    version="0.7.0",
     packages=['arglite'],
     package_dir={'arglite': 'src'},
     include_package_data=True,
