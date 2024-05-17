d0cd861868b4d1645b210e397b7575d231c997c6
dluman <dluman@allegheny.edu>
2024-05-17 18:20:30 -0400

    Sending up build files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="arglite",
-    version="0.7.5",
+    version="0.8.5",
     packages=['arglite'],
     package_dir={'': 'src'},
     include_package_data=True,
