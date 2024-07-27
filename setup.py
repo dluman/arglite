8ee62a6d7cee42e6dd1fa2bb932ff064706df1b7
dluman <dluman@allegheny.edu>
2024-07-27 11:08:54 -0400

    Updating to 0.9.0, restoring literal_eval functionality

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="arglite",
-    version="0.8.5",
+    version="0.9.0",
     packages=['arglite'],
     package_dir={'': 'src'},
     include_package_data=True,
