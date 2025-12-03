65d6dcdf36476fdf4db1ed8b29654ca9acd53b4d
dluman <dluman@allegheny.edu>
2025-12-02 20:42:32 -0500

    Updating with newer, probably more broken regex

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="arglite",
-    version="0.9.8",
+    version="0.9.9",
     packages=['arglite'],
     package_dir={'': 'src'},
     include_package_data=True,
